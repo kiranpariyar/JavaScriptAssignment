8e05d0ffa7b1da5992f9995cf80d9f66c3b4f56b
kiranpariyar <kirannpariyar@gmail.com>
2015-12-09 08:30:58 +0545

    simple changes

--- movingbox/js/Box.js
+++ movingbox/js/Box.js
@@ -2,8 +2,8 @@ var minX = 0;
 var minY = 0;
 var parentWidth = 1000;
 var parentHeight = 500;
-var boxWidth = 55;
-var boxHeight= 55;
+var boxWidth = 52;
+var boxHeight= 52;
 var maxX = minX + parentWidth - boxWidth ;
 var maxY = minY + parentHeight - boxHeight;
 var noOfCollision = 0; 
@@ -12,6 +12,8 @@ function Box(element) {
     this.element = element;
     this.xdir = Math.random() * (maxX - minX) + minX;
     this.ydir = Math.random() * (maxX - minX) + minX;
+//    this.xdir = 50;
+//    this.ydir = 10;
     this.SpeedX = 1;
     this.SpeedY = 1;
     
@@ -56,14 +58,72 @@ function Box(element) {
 	}
 
 	this.hitTest = function(box) {
-        
-//        console.log("this.element",this.xdir);
-//        console.log("box",box.xdir);
-        if(this.xdir < box.xdir + boxWidth && this.xdir + boxWidth > box.xdir
-           && this.ydir < box.ydir + boxHeight && this.ydir + boxHeight > box.ydir){
-            noOfCollision++;
-            console.log("collision detected",noOfCollision);
-            clearInterval(set);
-        }
+        var x = this.xdir;
+        var y = this.ydir;
+        var w = boxWidth;
+        var h = boxHeight;
+        var x1 = box.xdir;
+        var y1 = box.ydir;
+
+    if(this.xdir<box.xdir+boxWidth && this.xdir+boxWidth>box.xdir && this.ydir<box.ydir+boxHeight && this.ydir+boxHeight> box.ydir){
+//            noOfCollision++;
+//            console.log("collision detected",noOfCollision);
+            
+            
+            if((x+w)>=x1 && x<x1 && (y+h) > y1 && (y1+h)>=y){
+                console.log("zone 1 -right");
+                this.SpeedX = - this.SpeedX;
+                box.SpeedX = - box.SpeedX;
+                console.log("this.SpeedX",this.SpeedX);
+                console.log("box.SpeedX",box.SpeedX);
+            }else if((y+h)>=y1 && y<y1 && (x+w)>=x1 && x<(x1+w)){
+                console.log("zone 2 - bottom");
+                this.xdir += 2;
+                this.SpeedY = - this.SpeedY;
+                box.SpeedY = - box.SpeedY;
+//                clearInterval(set);
+            }else if((y1+h)>=y && y1<y && (x+w)>=x1 && x<(x1+w)){
+                console.log("zone 2 - top");
+                this.xdir += 2;
+                this.SpeedY = - this.SpeedY;
+                box.SpeedY = - box.SpeedY;
+            }else if((x1+w)>=x && x>x1 && y<(y1 + h) && (h + y)>y1){
+                console.log("zone 2 - left");
+                console.log("box.ydir+boxHeight",box.ydir,boxHeight);
+                console.log("this.ydir",this.ydir);
+                this.SpeedX = - this.SpeedX;
+                box.SpeedX = - box.SpeedX;
+//                clearInterval(set);
+            }
+            
+            
+//            clearInterval(set);
+                        
+//            var xdiff = box.xdir - this.xdir;
+//            var ydiff = box.ydir - this.ydir;
+//            var angle = Math.atan2(ydiff,xdiff) * 180/ Math.PI;
+//            console.log("angle ",angle);
+//            if (angle < 0) angle += 360;
+//            console.log("new angle ",angle);
+//            
+//            if ((angle >= 0 && angle < 45) || (angle > 315 && angle < 360)) {
+//                console.log("zone 1 - right");
+//            } else if (angle >= 45 && angle < 135) { 
+//                console.log("zone 2 - bottom");
+//                this.SpeedY = this.SpeedY;
+//                box.SpeedY = -box.SpeedX;
+//            } else if (angle >= 135 && angle < 225) { 
+//                console.log("zone 3 - left");
+//                this.SpeedX = this.SpeedX;
+//                box.SpeedX = - box.SpeedX;
+//            } else {
+//                console.log("zone 4 - top");
+//                this.SpeedY = - this.SpeedY;
+//                box.SpeedY = box.SpeedY;
+//            }   
+            
+    }else{
+        console.log("no collision");
+    }
 	}
 }
--- movingbox/js/index.js
+++ movingbox/js/index.js
@@ -9,10 +9,25 @@ function createBox(boxNumber){
     }
 }
 
-createBox(2);
+createBox(8);
 //console.log(mainDiv.children[0]);
 //console.log(mainDiv.children[1]);
 //console.log("mainDiv length :",mainDiv.children.length);
+
+
+
+//console.log("boxes:0",boxs[0]);
+//console.log("boxes:1",boxs[1]);
+
+//var box1 = new Box(mainDiv.children[0]);
+//box1.addClass("green");
+//var box2 = new Box(mainDiv.children[1]);
+//
+//var set = setInterval(function(){
+//    box1.moveBox();
+//    box2.moveBox();
+//    box1.hitTest(box2);
+//},10);
 
 var boxs = [];
 
@@ -20,24 +35,10 @@ for(var i = 0; i < mainDiv.childElementCount; i++){
     boxs.push(new Box(mainDiv.children[i]));
 }
 
-//console.log("boxes:0",boxs[0]);
-//console.log("boxes:1",boxs[1]);
-
-//var box1 = new Box(mainDiv.children[0]);
-//var box2 = new Box(mainDiv.children[1]);
-
-//setInterval(function(){
-//    box1.moveBox();
-//    box2.moveBox();
-//},10)
-
 var counter = 0;
 var set = window.setInterval(function(){
     for(var i = 0; i < boxs.length; i++){
         boxs[i].moveBox();
-    }
-    
-    for(var i = 0; i < boxs.length; i++){
         
         for(var j = 0; j != i && j < boxs.length;j++){
 ////          console.log("collision should not be detected with same box");
@@ -48,5 +49,5 @@ var set = window.setInterval(function(){
     
 },10);
 
-console.log("no. of hit test",counter);
+//console.log("no. of hit test",counter);
 
